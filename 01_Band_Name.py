ac931886349b98ffd9a33ea5a585a165bddd1d47
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2024-04-08 17:03:08 +0200

    01+02

---
+++ 01_Band_Name.py
@@ -0,0 +1,11 @@
+"""
+@Time ：08/04/2024 17:02
+@Auth ：LIN Tianyuan
+@File ：01_Band_Name.py
+@Motto：ABC(Always Be Coding)
+"""
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
