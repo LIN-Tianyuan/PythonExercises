c4ece5dee10409eef4734d8f5de8af89c2089d5c
Lucas <lucasyang515@gmail.com>
2025-05-03 15:42:41 +0200

    Lucas Y exercise 5

--- Submissions/Lucas Y/exercise5.py
+++ Submissions/Lucas Y/exercise5.py
@@ -1,4 +1,4 @@
-
+"""
 def filter_long_words(word_list, n):
     max_word_list = []
     max_word = max(len(word) for word in word_list)
@@ -8,6 +8,15 @@ def filter_long_words(word_list, n):
                 max_word_list.append(word)
     else:
         return 0
+    return max_word_list
+
+print(filter_long_words(["michael", "alex", "lucas", "luna", "coca"], 4))
+"""
+def filter_long_words(word_list, n):
+    max_word_list = []
+    for word in word_list:
+        if len(word) > n:
+            max_word_list.append(word)
     return max_word_list
 
 print(filter_long_words(["michael", "alex", "lucas", "luna", "coca"], 4))
