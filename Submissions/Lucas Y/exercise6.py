0586e054e852f38ca384f8f43ddf536498c1402a
Lucas <lucasyang515@gmail.com>
2025-05-12 21:08:15 +0200

    Lucas Y exercise 6

---
+++ Submissions/Lucas Y/exercise6.py
@@ -0,0 +1,17 @@
+def pangram(sentence):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet_list = []
+    sentence = sentence.lower()
+
+    for letter in sentence:
+        if letter in alphabet:
+            if letter not in alphabet_list:
+                alphabet_list.append(letter)
+
+    if len(alphabet_list) == 26:
+        print("It's a pangram.")
+    else:
+        print("It's not a pangram.")
+
+
+pangram("The quick brown fox jumps over the lazy dog.")
