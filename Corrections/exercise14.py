fcd4563f2c85555664a6814e1580cb020537ddbb
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2026-04-22 09:57:19 +0200

    New exercise 14

--- Corrections/exercise14.py
+++ Corrections/exercise14.py
@@ -41,3 +41,50 @@ Renvoyez la tête de la liste chaînée fusionnée.
 将这两个链表合并为一个已排序的链表。该链表应通过将前两个链表的节点拼接在一起生成。
 返回合并后链表的头节点。
 """
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def merge_two_lists(list1: ListNode, list2: ListNode) -> ListNode:
+    dummy = ListNode()   # 虚拟头节点
+    cur = dummy
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            cur.next = list1
+            list1 = list1.next
+        else:
+            cur.next = list2
+            list2 = list2.next
+        cur = cur.next
+
+    # 把没走完的那部分直接接上
+    cur.next = list1 if list1 else list2
+
+    return dummy.next
+
+if __name__ == '__main__':
+    def build_linked_list(arr):
+        dummy = ListNode()
+        cur = dummy
+        for num in arr:
+            cur.next = ListNode(num)
+            cur = cur.next
+        return dummy.next
+
+    def print_linked_list(head):
+        while head:
+            print(head.val, end=" -> ")
+            head = head.next
+        print("None")
+
+
+    list1 = build_linked_list([1,2,4])
+    list2 = build_linked_list([1,3,4])
+
+    result = merge_two_lists(list1, list2)
+
+    print_linked_list(result)
