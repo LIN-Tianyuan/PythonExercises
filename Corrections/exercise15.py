4d4c6a6ff061437ef6494e74b98124b13a7548a7
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2026-04-22 17:07:23 +0200

    New exercise 15

---
+++ Corrections/exercise15.py
@@ -0,0 +1,91 @@
+"""
+Leetcode 26: Remove Duplicates from Sorted Array
+English:
+Given an integer array nums sorted in non-decreasing order,
+remove the duplicates in-place such that each unique element appears only once.
+The relative order of the elements should be kept the same.
+
+Consider the number of unique elements in nums to be k.
+After removing duplicates, return the number of unique elements k.
+
+The first k elements of nums should contain the unique numbers in sorted order.
+The remaining elements beyond index k - 1 can be ignored.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-100 <= nums[i] <= 100
+nums is sorted in non-decreasing order.
+
+Français:
+Étant donné un tableau d'entiers nums trié par ordre non décroissant,
+supprimez les doublons sur place de manière à ce que chaque élément unique n'apparaisse qu'une seule fois.
+L'ordre relatif des éléments doit être conservé.
+
+中文：
+给定一个按非递减顺序排序的整数数组 nums，请就地移除重复项，使得每个唯一的元素只出现一次。元素的相对顺序应保持不变。
+"""
+
+"""
+解题思路：双指针
+
+因为数组已经排好序了，所以：
+
+重复的数字一定挨在一起
+
+所以我们可以用两个指针：
+
+slow：表示“当前去重后数组最后一个元素的位置”
+fast：往后扫描数组
+"""
+
+def removeDuplicates(nums):
+    if not nums:
+        return 0
+
+    slow = 0
+
+    for fast in range(1, len(nums)):
+        # 核心：说明发现了一个新数字，就把它放到 slow + 1 的位置。
+        if nums[fast] != nums[slow]:
+            slow += 1
+            nums[slow] = nums[fast]
+
+    return slow + 1
+
+if __name__ == '__main__':
+    nums = [0,0,1,1,1,2,2,3,3,4]
+    k = removeDuplicates(nums)
+    print(k)         # 5
+    print(nums[:k])  # [0, 1, 2, 3, 4]
