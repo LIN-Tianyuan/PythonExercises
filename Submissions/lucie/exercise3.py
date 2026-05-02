1fd2681a9a6202905f5a468fdf0701d3ade49263
lucie <lucielixiaoya1317@gmail.com>
2026-05-02 13:22:35 +0200

    Lucie exercise 3

---
+++ Submissions/lucie/exercise3.py
@@ -0,0 +1,24 @@
+"""
+English:
+Write a function find_longest_word() that takes a list of words
+and returns the length of the longest one.
+
+Français:
+Écrire une fonction find_longest_word() qui prend une liste de mots
+et renvoie la longueur du mot le plus long.
+
+中文：
+编写函数 find_longest_word()，接收单词列表并返回最长单词的长度。
+"""
+
+def find_longest_word(words):
+    if not words:
+        return 0
+    longest = 0
+    for word in words:
+        if len(word) > longest:
+            longest = len(word)
+    return longest
+word = ["CAca", "popi", "ziz","cacasjghazhdsg","🍧"]
+print(len(word))
+
