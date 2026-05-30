822979635c7a637e3377078472d6fed1bd1f1dd9
yixueliang <yixueliang0302@gmail.com>
2026-05-30 21:20:23 +0200

    exercise456 Yixue

---
+++ Submissions/Yixue/exercise5.py
@@ -0,0 +1,20 @@
+"""
+English:
+Write a function filter_long_words() that takes a list of words and
+an integer n and returns the list of words that are longer than n.
+
+Français:
+Écrire une fonction filter_long_words() qui prend une liste de mots et
+un entier n et renvoie la liste des mots qui sont plus longs que n.
+
+中文：
+编写一个函数 filter_long_words()，接收单词列表和整数 n，并返回长度大于 n 的单词列表。
+"""
+
+def filter_long_words(words,n):
+    l=[]
+    for i in words:
+        if len(i)>=n:
+            l.append(i)
+    return l
+print(filter_long_words(["b","bb","bbb","bbbb"],2))
