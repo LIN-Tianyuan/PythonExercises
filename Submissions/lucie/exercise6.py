76a3425a18866fe29f09afd39fce9a0f81dae418
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2026-05-30 13:38:42 +0200

    New exercise 20

--- Submissions/lucie/exercise6.py
+++ Submissions/lucie/exercise6.py
@@ -5,5 +5,6 @@ def est_un_pangramme(phrase):
     alphabet = set(string.ascii_lowercase)
     letters = set(phrase.lower())
     return alphabet.issubset(letters)
+
 print(est_un_pangramme("The quick brown fox jumps over the lazy dog."))
 print(est_un_pangramme("Bonjour à tous !"))
