77b559935ec41bc5d59b616cfe23e67616b433a4
lucie <lucielixiaoya1317@gmail.com>
2026-06-20 14:31:06 +0200

    Lucie exercise 9

--- Submissions/lucie/exercise 9.py
+++ Submissions/lucie/exercise 9.py
@@ -5,10 +5,18 @@ retourner leurs indices de tableau.
 """""
 
 def two_indices(nums, cible):
-
+    # nums= [2,11,15,7]
+    #cible=9
+    #len(nums)=4
+    #for i in range(4): O à 3
     for i in range(len(nums)):
+        #for j in range (i+1,4):1 à 4
         for j in range(i + 1, len(nums)):
+            #i =0
+            #num[j]=7
+            #cible= 9
             if nums[i] + nums[j] == cible:
+                #[0,1]
                 return [i, j]
     return None
 
