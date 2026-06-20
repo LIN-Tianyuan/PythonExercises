fef2fa1bc1c3cdcfc0406df364496f8bac00025b
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2026-06-20 14:26:39 +0200

    correction exercise 9

--- Submissions/lucie/exercise9.py
+++ Submissions/lucie/exercise9.py
@@ -5,14 +5,24 @@ retourner leurs indices de tableau.
 """""
 
 def two_indices(nums, cible):
-
+    # nums = [2, 11, 15, 7]
+    # cible = 9
+    # len(nums) = 4
+    # for i in range(4):  0 ~ 3
     for i in range(len(nums)):
+        # for j in range(i + 1, 4): 1 ~ 4
         for j in range(i + 1, len(nums)):
+            # i = 0
+            # nums[i] = 2
+            # j = 1
+            # nums[j] = 7
+            # cible = 9
             if nums[i] + nums[j] == cible:
+                # [0, 1]
                 return [i, j]
     return None
 
-liste = [2, 7, 11, 15]
+liste = [2, 11, 15, 7]
 cible = 9
 
 print(two_indices(liste, cible))
